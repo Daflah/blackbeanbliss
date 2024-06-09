21d1ffe4904471b855a1815183d70edf7db133ef
Daflah <daflah.t.gusra@gmail.com>
2024-06-09 13:13:26 +0700

    Firebase auth

---
+++ lib/wrapper.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:blackbeanbliss/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // return either Home or Authenticate widget
+    return Home();
+  }
+}

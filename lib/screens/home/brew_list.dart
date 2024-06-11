7ff40ca3d8e317623c0d4eafa9ed6e67410ce81b
Daflah <daflah.t.gusra@gmail.com>
2024-06-11 16:50:55 +0700

    Brew model, Listing brew data & Bottom sheets

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,3 +1,5 @@
+import 'package:blackbeanbliss/models/brew.dart';
+import 'package:blackbeanbliss/screens/home/brew_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,16 +14,15 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot?>(context);
+    final brews = Provider.of<List<Brew>>(context);
     
-    if (brews == null) {
-      return CircularProgressIndicator();
-    }
     
-    brews.docChanges.forEach((change) {
-      print(change.doc.data()); // Print data of each changed document
-    });
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
     
-    return Container();
+    );
   }
 }

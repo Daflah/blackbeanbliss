29a1d91e16e076c603f3dbf21be3f6608595120f
Daflah <daflah.t.gusra@gmail.com>
2024-06-11 21:27:01 +0700

    Stream Builder & updating user data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,11 +1,10 @@
 import 'package:blackbeanbliss/models/brew.dart';
 import 'package:blackbeanbliss/screens/home/brew_tile.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class BrewList extends StatefulWidget {
-  const BrewList({Key? key});
+  const BrewList({Key? key}) : super(key: key);
 
   @override
   State<BrewList> createState() => _BrewListState();
@@ -14,15 +13,15 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>>(context);
-    
-    
-    return ListView.builder(
-      itemCount: brews.length,
-      itemBuilder: (context, index) {
-        return BrewTile(brew: brews[index]);
-      },
-    
-    );
+    final brews = Provider.of<List<Brew>?>(context) ?? [];
+
+    return brews.isEmpty
+        ? Center(child: CircularProgressIndicator())  // Menampilkan indikator loading jika data belum tersedia
+        : ListView.builder(
+            itemCount: brews.length,
+            itemBuilder: (context, index) {
+              return BrewTile(brew: brews[index]);
+            },
+          );
   }
 }
--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,8 @@
+import 'package:blackbeanbliss/models/user.dart';
+import 'package:blackbeanbliss/services/database.dart';
 import 'package:blackbeanbliss/shared/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({super.key});
@@ -20,62 +23,80 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          const Text(
-            'Update your brew settings.',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          const SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration.copyWith(hintText: 'Name'),
-            validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          const SizedBox(height: 20.0),
-          DropdownButtonFormField(
-            decoration: textInputDecoration.copyWith(hintText: 'Sugars'),
-            value: _currentSugars,
-            items: sugars.map((sugar) {
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text('$sugar sugars'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _currentSugars = val as String?),
-          ),
-          const SizedBox(height: 20.0),
-          Slider(
-            value: (_currentStrength ?? 100).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-            min: 100,
-            max: 900,
-            divisions: 8,
-            onChanged: (val) => setState(() => _currentStrength = val.round()),
-          ),
-          const SizedBox(height: 20.0),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.pink[400],
-              onPrimary: Colors.white,
+
+    final user = Provider.of<User?>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user!.uid).userData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          UserData userData = snapshot.data as UserData;
+
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                const Text(
+                  'Update your brew settings.',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  initialValue: userData.name,
+                  decoration: textInputDecoration.copyWith(hintText: 'Name'),
+                  validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
+                  onChanged: (val) => setState(() => _currentName = val),
+                ),
+                const SizedBox(height: 20.0),
+                DropdownButtonFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Sugars'),
+                  value: _currentSugars ?? userData.sugars,
+                  items: sugars.map((sugar) {
+                    return DropdownMenuItem(
+                      value: sugar,
+                      child: Text('$sugar sugars'),
+                    );
+                  }).toList(),
+                  onChanged: (val) => setState(() => _currentSugars = val as String?),
+                ),
+                const SizedBox(height: 20.0),
+                Slider(
+                  value: (_currentStrength ?? userData.strength).toDouble(),
+                  activeColor: Colors.brown[_currentStrength ?? userData.strength],
+                  inactiveColor: Colors.brown[_currentStrength ?? userData.strength],
+                  min: 100,
+                  max: 900,
+                  divisions: 8,
+                  onChanged: (val) => setState(() => _currentStrength = val.round()),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.pink[400],
+                    onPrimary: Colors.white,
+                  ),
+                  child: const Text(
+                    'Update',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await DatabaseService(uid: user .uid).updateUserData(
+                        _currentSugars ?? userData.sugars,
+                        _currentName ?? userData.name,
+                        _currentStrength ?? userData.strength,
+                      );
+                      Navigator.pop(context);  // Close the settings form
+                    }
+                  },
+                ),
+              ],
             ),
-            child: const Text(
-              'Update',
-              style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () async {
-              if (_formKey.currentState!.validate()) {
-                print(_currentName);
-                print(_currentSugars);
-                print(_currentStrength);
-              }
-            },
-          ),
-        ],
-      ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
